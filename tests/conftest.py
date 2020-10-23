bc95f56cd51ccb6f1fa87c06de3f0548609a630b
Sivan Becker <sivanbecker@gmail.com>
2020-10-23 12:57:55 +0300

    pytest with postgres creation is working with pylint fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,12 @@
 import os
+from datetime import datetime
 import pytest
 import sqlalchemy as sa
-from datetime import datetime
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from pytest_postgresql.factories import DatabaseJanitor
 from psycopg2.errors import DuplicateDatabase
 
-from app import create_app
-from db import db 
+from app import create_app #pylint: disable=import-error
+from db import db #pylint: disable=import-error
 from models.provider import Provider
 
 class TestConfig:
@@ -17,15 +15,15 @@ class TestConfig:
     WTF_CSRF_ENABLED = False
     try:
         SQLALCHEMY_DATABASE_URI = os.environ['TEST_DATABASE_URL']
-    except KeyError:
+    except KeyError as exc:
         raise KeyError('TEST_DATABASE_URL not found. You must export a database ' +
                        'connection string to the environmental variable ' +
-                       'TEST_DATABASE_URL in order to run tests.')
+                       'TEST_DATABASE_URL in order to run tests.') from exc
     DB_OPTS = sa.engine.url.make_url(SQLALCHEMY_DATABASE_URI).translate_connect_args()
     try:
         POSTGRES_VER = os.environ['POSTGRES_VER']
-    except KeyError:
-        raise KeyError('Missing POSTGRES_VER env variable')
+    except KeyError as exc:
+        raise KeyError('Missing POSTGRES_VER env variable.') from exc
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
@@ -53,7 +51,7 @@ def database(request):
     pg_port = TestConfig.DB_OPTS.get("port")
     pg_db = TestConfig.DB_OPTS["database"]
     pg_pass = TestConfig.DB_OPTS.get("password")
-    janitor = DatabaseJanitor(pg_user, pg_host, pg_port, pg_db, TestConfig.POSTGRES_VER, pg_pass)    
+    janitor = DatabaseJanitor(pg_user, pg_host, pg_port, pg_db, TestConfig.POSTGRES_VER, pg_pass)
     try:
         janitor.init()
     except DuplicateDatabase:
@@ -61,12 +59,12 @@ def database(request):
         janitor.init()
 
     @request.addfinalizer
-    def drop_database():
+    def drop_database(): #pylint: disable=unused-variable
         janitor.drop()
 
 
 @pytest.fixture(scope='function')
-def _app(database):
+def _app(database): #pylint: disable=unused-argument, redefined-outer-name
     current_app = create_app()
     current_app.config['SQLALCHEMY_DATABASE_URI'] = TestConfig.SQLALCHEMY_DATABASE_URI
     current_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = TestConfig.SQLALCHEMY_TRACK_MODIFICATIONS
@@ -108,38 +106,3 @@ def client(_app, _db): #pylint: disable=redefined-outer-name, unused-argument
             _db.session.add(TestConfig.TEST_PROVIDER2)
             _db.session.commit()
             yield clnt
-
-
-# @pytest.fixture(scope='module')
-# def provider(request, _db):
-
-#     _db.create_all()
-
-#     @request.addfinalizer
-#     def drop_tables():
-#         _db.drop_all()
-
-
-
-# @pytest.fixture(scope='module')
-# def person(request, _db):
-#     '''
-#     Create a table to use for updating in the process of testing direct database access.
-#     '''
-#     class Person(_db.Model):
-#         __tablename__ = 'person'
-#         id = _db.Column(_db.Integer, primary_key=True)
-#         name = _db.Column(_db.String(80))
-
-#     # Create tables
-#     _db.create_all()
-
-#     @request.addfinalizer
-#     def drop_tables():
-#         _db.drop_all()
-
-#     return Person
-
-
-
-
