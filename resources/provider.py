630f86d4d4a8c546c84fd3ab46c84fd0458bc963
Sivan Becker <sivanbecker@gmail.com>
2020-11-01 21:06:04 +0200

    bugfix in provider udpate

--- resources/provider.py
+++ resources/provider.py
@@ -5,21 +5,23 @@ from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.exc import NoResultFound
 
 from models.provider import Provider
-from db import db #pylint: disable=import-error
+from db import db  # pylint: disable=import-error
 from schemas.provider import provider_schema, providers_schema
+
 
 class ProviderResource(Resource):
 
     BASE_ROUTE = '/provider'
     API_ROUTE = '/provider/<string:mispar_osek>'
-    def get(self, mispar_osek): #pylint: disable=no-self-use
+
+    def get(self, mispar_osek):  # pylint: disable=no-self-use
         try:
             _provider = Provider.query.filter_by(mispar_osek=mispar_osek).one()
             return {'provider': provider_schema.dump(_provider)}
         except NoResultFound:
             return {'provider': None}, 404
 
-    def post(self, mispar_osek): #pylint: disable=no-self-use
+    def post(self, mispar_osek):  # pylint: disable=no-self-use
         if Provider.query.filter_by(mispar_osek=mispar_osek).count():
             return {'message': f'Provider with mispar-osek={mispar_osek} already exists'}, 400
 
@@ -37,13 +39,14 @@ class ProviderResource(Resource):
         except IntegrityError as exc:
             return {'message': str(exc)}, 400
 
-
-    def put(self, mispar_osek): #pylint: disable=no-self-use
+    def put(self, mispar_osek):  # pylint: disable=no-self-use
         data = request.get_json()
         try:
             _provider = Provider.query.filter_by(mispar_osek=mispar_osek).one()
             _provider.service_type = data['service_type']
             _provider.name = data['name']
+            db.session.add(_provider)
+            db.session.commit()
             return {'message': 'Provider updated'}
         except NoResultFound:
             new_provider = Provider(
@@ -56,7 +59,7 @@ class ProviderResource(Resource):
             db.session.commit()
             return {'provider': provider_schema.dump(new_provider)}, 201
 
-    def delete(self, mispar_osek): #pylint: disable=no-self-use
+    def delete(self, mispar_osek):  # pylint: disable=no-self-use
         try:
             _provider = Provider.query.filter_by(mispar_osek=mispar_osek).one()
             db.session.delete(_provider)
@@ -65,9 +68,11 @@ class ProviderResource(Resource):
         except NoResultFound:
             return {'message': f'Provider with mispar-osek={mispar_osek} does not exist'}, 400
 
+
 class ProviderListResource(Resource):
 
     BASE_ROUTE = '/providers'
     API_ROUTE = '/providers'
-    def get(self): #pylint: disable=no-self-use
+
+    def get(self):  # pylint: disable=no-self-use
         return {'providers': providers_schema.dump(Provider.query.all())}
